b69b183e7ed07b9ad94e8f2b92cfc14b1ca604f5
alfagov <lorenzopulcini@protonmail.ch>
2024-12-03 01:14:19 +0100

    Refactor Day1-2

--- src/day1.zig
+++ src/day1.zig
@@ -1,64 +1,75 @@
 const std = @import("std");
 const Utils = @import("utils.zig");
 
+const Part2Processor = struct {
+    lhs: *std.AutoArrayHashMap(u32, u32),
+    rhs: *std.AutoArrayHashMap(u32, u32),
+
+    const Self = @This();
+
+    pub fn getCallback(self: *Self) Utils.AccumulatorCallback {
+        return .{
+            .ptr = self,
+            .callbackFn = callback,
+        };
+    }
+
+    pub fn callback(ptr: *anyopaque, line: []const u8) !void {
+        var self: *Self = @ptrCast(@alignCast(ptr));
+
+        const left = parseInt(line[0..5]);
+        const right = parseInt(line[8..]);
+
+        if (self.lhs.getEntry(left)) |entry| {
+            entry.value_ptr.* = entry.value_ptr.* + 1;
+        } else {
+            self.lhs.putAssumeCapacity(left, 1);
+        }
+
+        if (self.rhs.getEntry(right)) |entry| {
+            entry.value_ptr.* = entry.value_ptr.* + 1;
+        } else {
+            self.rhs.putAssumeCapacity(right, 1);
+        }
+    }
+
+    pub fn calculateDistance(self: *Self) usize {
+        var acc: u32 = 0;
+        var l_iterator = self.lhs.iterator();
+
+        while (l_iterator.next()) |l_value| {
+            if (self.rhs.get(l_value.key_ptr.*)) |r_v| {
+                acc += l_value.key_ptr.* * l_value.value_ptr.* * r_v;
+            }
+        }
+
+        return acc;
+    }
+};
+
 pub fn part2() !void {
     const start_time = std.time.microTimestamp();
-
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
 
-    const file = try std.fs.cwd().openFile("data/input.txt", .{});
-    defer file.close();
-
-    var buf_reader = std.io.bufferedReader(file.reader());
-    var in_stream = buf_reader.reader();
-
     var left = std.AutoArrayHashMap(u32, u32).init(gpa.allocator());
     defer left.deinit();
+    try left.ensureTotalCapacity(1000);
     var right = std.AutoArrayHashMap(u32, u32).init(gpa.allocator());
     defer right.deinit();
+    try right.ensureTotalCapacity(1000);
 
-    var buf: [1024]u8 = undefined;
-    while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
-        var line_stream = std.io.fixedBufferStream(line);
-        var line_reader = line_stream.reader();
+    var processor = Part2Processor{ .lhs = &left, .rhs = &right};
+    var file_reader = try Utils.FileLineReaderAccumulator.init("data/input.txt");
 
-        var left_num_buf: [10]u8 = undefined;
-        var right_num_buf: [10]u8 = undefined;
-
-        const left_num = try line_reader.readUntilDelimiter(&left_num_buf, ' ');
-        _ = try line_reader.readByte();
-        _ = try line_reader.readByte();
-        const right_num = try line_reader.readUntilDelimiterOrEof(&right_num_buf, ' ');
-        const l = try std.fmt.parseInt(u32, left_num, 10);
-        const r = try std.fmt.parseInt(u32, right_num.?, 10);
-
-        if (left.get(l)) |l_v| {
-            try left.put(l, l_v + 1);
-        } else {
-            try left.put(l, 1);
-        }
-
-        if (right.get(r)) |r_v| {
-            try right.put(r, r_v + 1);
-        } else {
-            try right.put(r, 1);
-        }
-    }
-
-    var acc: u32 = 0;
-    var l_iterator = left.iterator();
-
-    while (l_iterator.next()) |l_value| {
-        if (right.get(l_value.key_ptr.*)) |r_v| {
-            acc += l_value.key_ptr.* * l_value.value_ptr.* * r_v;
-        }
-    }
+    try file_reader.readFile(processor.getCallback());
+    const distance = processor.calculateDistance();
 
     const end_time = std.time.microTimestamp();
     std.debug.print("Elapsed time: {d:.3} ms\n", .{@as(f64, @floatFromInt(end_time - start_time)) / 1000.0});
-    std.debug.print("Result: {}\n", .{acc});
+    std.debug.print("Result: {}\n", .{distance});
 }
+
 
 const Part1Processor = struct {
     head: usize = 0,
