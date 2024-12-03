3d9faab430617ecd0769372575bb712220a9712e
alfagov <lorenzopulcini@protonmail.ch>
2024-12-03 12:46:16 +0100

    Day 3 perm tests

--- src/day3.zig
+++ src/day3.zig
@@ -20,59 +20,58 @@ const Part2Processor = struct {
         var line_buf: []u8 = @constCast(line[0..]);
         wh: while (line_buf.len > 0) {
             const c = line_buf[0];
-
-            if (c == 'd') {
-                if (std.mem.eql(u8, line_buf[0..4], "do()")) {
-                    line_buf = line_buf[4..];
-                    self.mul_enable = true;
-                } else if (std.mem.eql(u8, line_buf[0..7], "don't()")) {
-                    line_buf = line_buf[7..];
-                    self.mul_enable = false;
-                } else {
-                    line_buf = line_buf[1..];
-                }
-            } else if (c == 'm') {
-                if (std.mem.eql(u8, line_buf[0..4], "mul(")) {
+            switch (c) {
+                'd' => {
+                    if (std.mem.indexOfDiff(u8, line_buf[0..4], "do()")) |_| {
+                        if (std.mem.indexOfDiff(u8, line_buf[0..7], "don't()")) |diff_idx| {
+                            line_buf = line_buf[diff_idx..];
+                        } else {
+                            line_buf = line_buf[7..];
+                            self.mul_enable = false;
+                        }
+                    }  else {
+                        line_buf = line_buf[4..];
+                        self.mul_enable = true;
+                    }
+                },
+                'm' => {
                     var num1: u32 = 0;
                     var num2: u32 = 0;
 
-                    for (line_buf[4..], 0..) |value, idx| {
-                        //std.debug.print("V {c}\n", .{value});
-                        if (std.ascii.isDigit(value)) {
-                            num1 = num1 * 10 + (value - '0');
-                        } else if (value == ',') {
-                            line_buf = line_buf[5 + idx ..];
-                            break;
-                        } else {
-                            line_buf = line_buf[4..];
-                            continue :wh;
+                    if (std.mem.indexOfDiff(u8, line_buf[0..4], "mul(")) |diff_idx| {
+                        line_buf = line_buf[diff_idx..];
+                    } else {
+                        line_buf = line_buf[4..];
+                        for (line_buf[0..], 0..) |value, idx| {
+                            if (std.ascii.isDigit(value)) {
+                                num1 = num1 * 10 + (value - '0');
+                            } else if (value == ',') {
+                                line_buf = line_buf[idx + 1 ..];
+                                break;
+                            } else {
+                                line_buf = line_buf[idx..];
+                                continue :wh;
+                            }
+                        }
+
+                        for (line_buf[0..], 0..) |value, idx| {
+                            if (std.ascii.isDigit(value)) {
+                                num2 = num2 * 10 + (value - '0');
+                            } else if (value == ')') {
+                                line_buf = line_buf[idx + 1 ..];
+                                break;
+                            } else {
+                                line_buf = line_buf[idx..];
+                                continue :wh;
+                            }
+                        }
+
+                        if (self.mul_enable) {
+                            self.accumulator += num1 * num2;
                         }
                     }
-
-                    for (line_buf[0..], 0..) |value, idx| {
-                        if (std.ascii.isDigit(value)) {
-                            num2 = num2 * 10 + (value - '0');
-                        } else if (value == ')') {
-                            line_buf = line_buf[idx + 1 ..];
-                            break;
-                        } else {
-                            line_buf = line_buf[1..];
-                            continue :wh;
-                        }
-                    }
-
-                    if (self.mul_enable) {
-                        self.accumulator += num1 * num2;
-                    }
-
-                    //std.debug.print("({},{})\n", .{ num1, num2 });
-                } else {
-                    //std.debug.print("NOT {c}\n", .{c});
-                    line_buf = line_buf[1..];
-                }
-            } else {
-                //std.debug.print("NOT {c}\n", .{c});
-                line_buf = line_buf[1..];
+                },
+                else => line_buf = line_buf[1..],
             }
         }
     }
@@ -97,14 +96,12 @@ const Part1Processor = struct {
         wh: while (line_buf.len > 0) {
             const c = line_buf[0];
 
-            //std.debug.print("CHAR {c}\n", .{c});
             if (c == 'm') {
                 if (std.mem.eql(u8, line_buf[0..4], "mul(")) {
                     var num1: u32 = 0;
                     var num2: u32 = 0;
 
                     for (line_buf[4..], 0..) |value, idx| {
-                        //std.debug.print("V {c}\n", .{value});
                         if (std.ascii.isDigit(value)) {
                             num1 = num1 * 10 + (value - '0');
                         } else if (value == ',') {
@@ -129,13 +126,10 @@ const Part1Processor = struct {
                     }
 
                     self.accumulator += num1 * num2;
-                    //std.debug.print("({},{})\n", .{ num1, num2 });
                 } else {
-                    //std.debug.print("NOT {c}\n", .{c});
                     line_buf = line_buf[1..];
                 }
             } else {
-                //std.debug.print("NOT {c}\n", .{c});
                 line_buf = line_buf[1..];
             }
         }
